8a74027d8ddf3ae0b0ea3977c04398b760d1760b
2unthoughtknown-hash <2unthought.known0907@gmail.com>
2025-08-12 18:42:35 +0900

    Update AppManageSdk.kt

--- AppManageSdk.kt
+++ AppManageSdk.kt
@@ -3,3 +3,15 @@
         val response = binder.ensureServiceConnected().setDeliverFileInfo(deliverFileInfo)
         return parseApiResponse(response, Unit::class.java)
     }
+
+      override suspend fun updateDeliverFileInfo(
+        dataType: String,
+        version: String,
+        filePath: String,
+        downloadStatus: String,
+        downloadDateTime: String
+    ): Result<Unit> {
+        LogUtil.i(TAG, "updateDeliverFileInfo() called with dataType: $dataType, version: $version, filePath: $filePath, downloadStatus: $downloadStatus, downloadDateTime: $downloadDateTime")
+        val response = binder.ensureServiceConnected().updateDeliverFileInfo(dataType, version, filePath, downloadStatus, downloadDateTime)
+        return parseApiResponse(response, Unit::class.java)
+    }
